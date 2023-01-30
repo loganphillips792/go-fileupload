411087924f8ee7e746b41c47947b64eaed29ff07
loganphillips792 <loganphillips792@gmail.com>
2023-01-30 16:17:16 -0500

    create users table

--- backend/script.sql
+++ backend/script.sql
@@ -5,3 +5,5 @@ CREATE TABLE IF NOT EXISTS images (id INTEGER PRIMARY KEY AUTOINCREMENT, name TE
 INSERT INTO images (name, file_path) VALUES ('Name 1', 'Name 1');
 INSERT INTO images (name, file_path) VALUES ('Name 2', 'Name 2');
 INSERT INTO images (name, file_path) VALUES ('Name 3', 'Name 3');
+
+CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, email TEXT, password TEXT)

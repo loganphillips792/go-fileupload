65467f6083efa85c57b88c855f8766fe61ad8348
loganphillips792 <loganphillips792@gmail.com>
2023-02-06 17:18:36 -0500

    scripts

--- backend/script.sql
+++ backend/script.sql
@@ -1,10 +1,9 @@
 DROP TABLE IF EXISTS images;
 
 CREATE TABLE IF NOT EXISTS images (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, file_path TEXT);
+CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, email TEXT, password TEXT);
+CREATE TABLE IF NOT EXISTS sessions (session_id TEXT PRIMARY KEY, expires_at INTEGER);
 
 INSERT INTO images (name, file_path) VALUES ('Name 1', 'Name 1');
 INSERT INTO images (name, file_path) VALUES ('Name 2', 'Name 2');
 INSERT INTO images (name, file_path) VALUES ('Name 3', 'Name 3');
-
-CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, email TEXT, password TEXT);
-CREATE TABLE IF NOT EXISTS sessions (session_id TEXT PRIMARY KEY, expires_at INTEGER);
